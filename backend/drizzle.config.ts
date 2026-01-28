87f37b1f034caf7a631a172932fdd758442253d3
enyan27 <truongdat2325@gmail.com>
2026-01-28 12:15:55 +0700

    feat: add drizzle schema setup

---
+++ backend/drizzle.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "drizzle-kit";
+import { ENV } from "./src/config/env";
+
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: { url: ENV.DATABASE_URL! },
+  out: "./src/db/migrations",
+});
